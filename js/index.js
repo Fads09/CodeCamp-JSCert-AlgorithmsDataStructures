a1c63632e14036bbe16987681930073a48228cd1
Fadoua Amrou <faitamrou.90@gmail.com>
2019-05-09 00:00:10 -0400

    Initial Commit
    * Basic Website structure
    * Shows the output in the console that  is needed
    * Exported from codepen

---
+++ js/index.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+// Basic JavaScript: Use Bracket Notation to Find the Nth-to-Last Character in a String
+var firstName = "Ada";
+var thirdToLastLetterOfFirstName = firstName[firstName.length - 3];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line
+var secondToLastLetterOfLastName = lastName[lastName.length -2];
+console.log(secondToLastLetterOfLastName);
+});
