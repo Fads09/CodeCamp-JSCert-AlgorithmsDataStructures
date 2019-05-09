051a6e484c9f6212ebeace71c176815e40a0c523
Fadoua Amrou <faitamrou.90@gmail.com>
2019-05-09 00:07:50 -0400

    Assignment 36 complete
    * Basic JavaScript: Use Bracket Notation to Find the Nth-to-Last Character in a String

--- js/index.js
+++ js/index.js
@@ -1,12 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
-// Basic JavaScript: Use Bracket Notation to Find the Nth-to-Last Character in a String
-var firstName = "Ada";
-var thirdToLastLetterOfFirstName = firstName[firstName.length - 3];
+    Assignment37();
+    Assignment36();
+});
+function Assignment37()
+{}
+
+function Assignment36()
+{
+    // Assignment Title: Basic JavaScript: Use Bracket Notation to Find the Nth-to-Last Character in a String
+    var firstName = "Ada";
+    var thirdToLastLetterOfFirstName = firstName[firstName.length - 3];
 
 // Setup
-var lastName = "Lovelace";
+    var lastName = "Lovelace";
 
 // Only change code below this line
-var secondToLastLetterOfLastName = lastName[lastName.length -2];
-console.log(secondToLastLetterOfLastName);
-});
+    var secondToLastLetterOfLastName = lastName[lastName.length -2];
+    console.log(secondToLastLetterOfLastName);
+}
